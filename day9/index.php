3b3b1ec91fac41e258791d59d81b8c77ed9b2436
Aldin Wildan <08.aldinwildan@gmail.com>
2024-04-05 01:04:17 +0700

    menyelesaikan soal untuk release 1

--- day9/index.php
+++ day9/index.php
@@ -1,6 +1,26 @@
 <?php
-require('animal.php');
+
+// require('animal.php');
+require('other.php');
+
 $sheep = new Animal("shaun"); //penambahan object baru untuk class Animal dengan name Shaun menggunakan construct
+
 echo "nama hewan: " . $sheep->name . "<br>"; // "shaun"
 echo "jumlah kaki: " . $sheep->legs . "<br>"; // 4
-echo "tipe darah: " . $sheep->cold_blooded . "<br>"; // "no"
+echo "cold blooded: " . $sheep->cold_blooded . "<br>"; // "no"
+echo  "<hr>";
+
+$kodok = new frog("buduk");
+
+echo "nama hewan: " . $kodok->name . "<br>";
+echo "jumlah kaki: " . $kodok->legs . "<br>";
+echo "cold blooded: " . $kodok->cold_blooded . "<br>";
+$kodok->jump();
+echo  "<hr>";
+
+$sungokong = new ape("kera sakti");
+
+echo "nama hewan: " . $sungokong->name . "<br>";
+echo "jumlah kaki: " . $sungokong->legs . "<br>";
+echo "cold blooded: " . $sungokong->cold_blooded . "<br>";
+$sungokong->yell();
